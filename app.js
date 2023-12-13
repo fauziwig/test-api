9b919972c271963b3a1d0d93f72ce9439b90b9d5
fauziwig <triagungwiguna@gmail.com>
2023-12-13 13:30:29 +0700

    updated file

--- app.js
+++ app.js
@@ -1,78 +1,115 @@
-const Hapi = require('@hapi/hapi');
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
 
-const users = ["Bejo", "Paijo", "Rudi"]
-const foods = ["Soto", "Burger", "Nasi Goreng"]
-const karyawan = {
-    nama : "wiguna",
-    jabatan : "senior manager",
-    gaji : "9 digit"
-}
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'db_coba'
+});
 
-const init = async () => {
-  const server = Hapi.server({
-    port: 3000,
-    host: 'localhost'
-  });
-
-  //endpoint utama
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (request, h) => {
-      return 'Halo, ini adalah API sederhana menggunakan Hapi.js';
-    }
-  });
-
-  //endpoint get all data users
-  server.route({
-    method: 'GET',
-    path: '/api/users',
-    handler: (request, h) => {
-      console.log(users)
-      return { users };
-    }
-  });
-
-  //endpoint post tambah data users
-  server.route({
-    method: 'POST',
-    path: '/api/users',
-    handler: (request, h) => {
-      const { name, email } = request.payload;
-      const newUser = { id: users.length + 1, name, email };
-      users.push(newUser);
-      return { message: 'Pengguna berhasil ditambahkan', user: newUser };
-    }
-  });
-
-  //endpoint get all data makanan
-  server.route({
-    method: 'GET',
-    path: '/api/foods',
-    handler: (request, h) => {
-      return { foods };
-    }
-  });
-
-  //endpoint post tambah data makanan 
-  server.route({
-    method: 'POST',
-    path: '/api/foods',
-    handler: (request, h) => {
-      const { name, type } = request.payload;
-      const newFood = { id: foods.length + 1, name, type };
-      foods.push(newFood);
-      return { message: 'Makanan berhasil ditambahkan', food: newFood };
-    }
-  });
-
-
-  try {
-    await server.start();
-    console.log('Server berjalan pada', server.info.uri);
-  } catch (err) {
-    console.log('Error saat menjalankan server:', err);
+connection.connect((err) => {
+  if (err) {
+    console.error('Koneksi ke database gagal: ', err);
+    return;
   }
-};
+  console.log('Koneksi ke database berhasil');
+});
 
-init();
+// const users = ["Bejo", "Paijo", "Rudi"];
+// const foods = ["Soto", "Burger", "Nasi Goreng"];
+
+// const karyawan = {
+//     nama: "wiguna",
+//     jabatan: "senior manager",
+//     gaji: "9 digit"
+// };
+
+// Endpoint for root
+app.get('/', (req, res) => {
+    res.send('Halaman utama');
+});
+
+// Endpoint to get all users
+app.get('/api/users', (req, res) => {
+  connection.query('SELECT * FROM users', (error, results) => {
+    if (error) {
+      console.error('Error saat mengambil data pengguna: ', error);
+      res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data' });
+      return;
+    }
+    res.send({ users: results });
+  });
+});
+
+app.get('/api/users/:id', (req, res) => {
+  const id = req.params.id
+  connection.query(`SELECT * FROM users WHERE id = ${id}`, (error, results) => {
+    if (error) {
+      console.error('Error saat mengambil data pengguna: ', error);
+      res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data' });
+      return;
+    }
+    // res.json(`hasil ${id}`)
+    res.json({results})
+  });
+});
+
+
+app.get('/api/users/search', (req, res) => {
+  console.log("Mencari id :", req.query.id)
+
+  connection.query( `SELECT * FROM users WHERE id = ${req.query.id}`, (error, results) => {
+    if (error) {
+      console.error('Error saat mengambil data pengguna: ', error);
+      res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data' });
+      return;
+    }
+    res.json({ users: results });
+  });
+});
+
+// Endpoint to add a new user
+app.post('/api/users', (req, res) => {
+    const { name, email } = req.body;
+    const newUser = { id: users.length + 1, name, email };
+    users.push(newUser);
+    res.json({ message: 'Pengguna berhasil ditambahkan', user: newUser });
+});
+
+// Endpoint to get all foods
+app.get('/api/food', (req, res) => {
+  connection.query('SELECT * FROM food', (error, results) => {
+    if (error) {
+      console.error('Error saat mengambil data pengguna: ', error);
+      res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data' });
+      return;
+    }
+    res.json({ users: results });
+  });
+});
+
+// Endpoint to add a new food
+app.post('/api/foods', (req, res) => {
+    const { name, type } = req.body;
+    const newFood = { id: foods.length + 1, name, type };
+    foods.push(newFood);
+    res.json({ message: 'Makanan berhasil ditambahkan', food: newFood });
+});
+
+app.get('/api/restaurants', (req, res) => {
+  connection.query('SELECT * FROM restaurants', (error, results) => {
+    if (error) {
+      console.error('Error saat mengambil data pengguna: ', error);
+      res.status(500).json({ error: 'Terjadi kesalahan saat mengambil data' });
+      return;
+    }
+    res.json({ users: results });
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server berjalan pada http://localhost:${port}`);
+});
